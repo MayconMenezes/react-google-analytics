918e679ed139f92ff9e9bad889b642bd37a8bb91
MayconMenezes <ap_maycon@hotmail.com>
2021-03-04 20:05:19 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { checkSignedIn, renderButton } from "./utils";
 
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const updateSignin = (signedIn) => {
+    setIsSignedIn(signedIn);
+    if (!signedIn) {
+      renderButton();
+    }
+  };
+
+  const init = () => {
+    checkSignedIn()
+      .then((signedIn) => {
+        updateSignin(signedIn);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    window.gapi.load("auth2", init); //(1)
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isSignedIn ? <div id="signin-button"></div> : <div>Coming soon...</div>}
     </div>
   );
 }
---
+++ src/utils.js
@@ -0,0 +1,74 @@
+const initAuth = () => {
+  return window.gapi.auth2.init({
+    client_id: "YOUR_CLIENT_ID", //paste your client ID here
+    scope: "https://www.googleapis.com/auth/analytics.readonly",
+  });
+};
+
+const checkSignedIn = () => {
+  return new Promise((resolve, reject) => {
+    initAuth() //calls the previous function
+      .then(() => {
+        const auth = window.gapi.auth2.getAuthInstance(); //returns the GoogleAuth object
+        resolve(auth.isSignedIn.get()); //returns whether the current user is currently signed in
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+const renderButton = () => {
+  window.gapi.signin2.render("signin-button", {
+    scope: "profile email",
+    width: 240,
+    height: 50,
+    longtitle: true,
+    theme: "dark",
+    onsuccess: onSuccess,
+    onfailure: onFailure,
+  });
+};
+
+const onSuccess = (googleUser) => {
+  console.log("Logged in as: " + googleUser.getBasicProfile().getName());
+};
+
+const onFailure = (error) => {
+  console.error(error);
+};
+
+const startClientHelper = () => {
+  // 2. Initialize the JavaScript client library.
+  window.gapi.client
+    .init({
+      apiKey: "YOUR_API_KEY",
+      // Your API key will be automatically added to the Discovery Document URLs.
+      discoveryDocs: ["https://people.googleapis.com/$discovery/rest"],
+      // clientId and scope are optional if auth is not required.
+      clientId: "YOUR_WEB_CLIENT_ID.apps.googleusercontent.com",
+      scope: "profile",
+    })
+    .then(function () {
+      // 3. Initialize and make the API request.
+      return window.gapi.client.people.people.get({
+        resourceName: "people/me",
+        "requestMask.includeField": "person.names",
+      });
+    })
+    .then(
+      function (response) {
+        console.log(response.result);
+      },
+      function (reason) {
+        console.log("Error: " + reason.result.error.message);
+      }
+    );
+};
+
+const getClientId = () => {
+  // 1. Load the JavaScript client library.
+  window.gapi.load("client", startClientHelper);
+};
+
+export { checkSignedIn, renderButton, getClientId };
